7d949ede453c8616f1738c33432c3f1da3617fac
AlexJPF <45736019+AlexJPF@users.noreply.github.com>
2021-01-10 19:56:19 -0600

    Add files via upload

---
+++ degree.h
@@ -0,0 +1,15 @@
+//
+// JYM1 - degree.h
+// Scripting and Programming Applications - C867
+// Alexander J. Pfleging
+// Student ID# 000821291
+//
+
+#pragma once
+
+//
+
+enum DegreeProgram{SECURITY, NETWORK, SOFTWARE, UNDECIDED};
+
+static const std::string degreeProgramArray[] =
+    { "Security", "Network", "Software", "Undecided" };
